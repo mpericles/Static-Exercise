67447f3bce60e9bb1e13c5ef9f047dbcba61ce62
mpericles <mpericles1@yahoo.com>
2023-07-05 21:32:14 -0400

    Static Exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -2,9 +2,27 @@
 {
     public class Program
     {
+        //Now create a static class called TempConverter. The class should have at least 2 methods,
+        //one called FahrenheitToCelsius that will require a double as a
+        //parameter and return a double, the other CelsiusToFahrenheit which will also require a double
+        //as a parameter and return a double.
+        //Fill out these methods and call them in your Program.cs file. Make sure they’re accurate!
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Call First Method
+            Console.WriteLine(" ");
+            Console.WriteLine("Please enter Farenheit Temp");
+            var temp1 = double.Parse(Console.ReadLine());
+            TempConverter.FahrenheitToCelsius(temp1);
+
+            //Call Second Method
+            Console.WriteLine(" ");
+            Console.WriteLine("Please enter Celsuis Temp");
+            var temp2 = double.Parse(Console.ReadLine());
+            TempConverter.CelsiusToFahrenheit(temp2);
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        
+        {
+            // Formula : °C = 5/9(°F – 32
+            // Example Given, F = 80°|| Using the formula, °C = 5/9(°F – 32) ||
+            // C = 5/9(80 – 32) || C = 26.67°C || Hence, 80°F = 26.67°C
+
+            // Convert Fahrenheit to Celsius
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine($"{fahrenheit} Farenheit is {celsius} Celsuis");
+            return celsius;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            // Formula : (0°C × 9/5) + 32 = 32°F
+            // Example : (50°F - 32) x .5556 = 10°C
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine($"{celsius} Celsuis is {fahrenheit} Farenheit");
+            return fahrenheit ;
+        }
+    }
+}
